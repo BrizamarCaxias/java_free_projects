129ce8a08d3d8fa9d5912de0907067b21e1a3490
Brizamar Caxias <brizamarcaxias@gmail.com>
2023-04-05 21:22:37 -0300

    chore: add devtools and modifying of Controller to RestController.

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,14 +3,12 @@ import br.com.alura.forum.modelo.*;
 import java.util.*;
 
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TopicosController {
 	@RequestMapping("/topicos")
-	@ResponseBody
 	public List<Topico> lista(){
 		
 		Topico topico = new Topico("Dúvida","Dúvida com Spring",new Curso("Spring","Programação"));
