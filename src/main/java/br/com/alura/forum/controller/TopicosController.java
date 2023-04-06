c00bb3af5d085eabffb909818441b24ea9e620d1
Brizamar Caxias <brizamarcaxias@gmail.com>
2023-04-05 22:36:56 -0300

    chore:includ class DTO for Topicos.

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,4 +1,5 @@
 package br.com.alura.forum.controller;
+import br.com.alura.forum.dto.TopicoDTO;
 import br.com.alura.forum.modelo.*;
 import java.util.*;
 
@@ -9,10 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TopicosController {
 	@RequestMapping("/topicos")
-	public List<Topico> lista(){
+	public List<TopicoDTO> lista(){
 		
 		Topico topico = new Topico("Dúvida","Dúvida com Spring",new Curso("Spring","Programação"));
-		return Arrays.asList(topico,topico,topico);
+		return TopicoDTO.converte(Arrays.asList(topico,topico,topico,topico));
 		
 	}
 	
