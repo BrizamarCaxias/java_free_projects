bdfaba7e7eccf815f2ca46b2777986522f5389cd
Brizamar Caxias <brizamarcaxias@gmail.com>
2023-04-06 15:14:11 -0300

    chore: add data.sql and configuration of class entity.

--- src/main/java/br/com/alura/forum/modelo/Curso.java
+++ src/main/java/br/com/alura/forum/modelo/Curso.java
@@ -1,9 +1,16 @@
 package br.com.alura.forum.modelo;
 
-public class Curso {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class Curso {
+	@Id@GeneratedValue(strategy = GenerationType.IDENTITY )
 	private Long id;
 	private String nome;
+	
 	private String categoria;
 
 	public Curso(String nome, String categoria) {
