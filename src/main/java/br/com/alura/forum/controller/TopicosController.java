3c4252b4d5a9e9c609d7a3367ad5f5e552c27141
Brizamar Caxias <brizamarcaxias@gmail.com>
2023-04-07 14:39:09 -0300

    chore: implements of search by nome curso.

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,19 +1,23 @@
 package br.com.alura.forum.controller;
 import br.com.alura.forum.dto.TopicoDTO;
 import br.com.alura.forum.modelo.*;
+import br.com.alura.forum.repository.TopicoRepository;
+
 import java.util.*;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicosController {
+	@Autowired
+	private TopicoRepository topicoRepository;
 	@RequestMapping("/topicos")
-	public List<TopicoDTO> lista(){
+	public List<TopicoDTO> lista(String nomeCurso){
 		
-		Topico topico = new Topico("Dúvida","Dúvida com Spring",new Curso("Spring","Programação"));
-		return TopicoDTO.converte(Arrays.asList(topico,topico,topico,topico));
+		List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+		return TopicoDTO.converte(topicos);
 		
 	}
 	
